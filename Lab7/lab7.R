bf6fdba81a6b1bce18140a89ca04699b5de572a5
miche <michelle.dardon@ufm.edu>
2024-10-19 22:30:21 -0600

    Create lab7.R

---
+++ Lab7/lab7.R
@@ -0,0 +1,5 @@
+library(readr)
+Health_and_Personal_Care_metadata <- read_csv("C:/Users/miche/Downloads/Archive/Health_and_Personal_Care_metadata.csv")
+Health_and_Personal_Care <- read_csv("C:/Users/miche/Downloads/Archive/Health_and_Personal_Care.csv")
+
+
